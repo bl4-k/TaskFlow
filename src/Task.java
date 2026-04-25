379f53359b9c9cc071f09f1fe8ce1a8ddf31a205
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-25 01:18:58 +0100

    feature: added task class

---
+++ src/Task.java
@@ -0,0 +1,39 @@
+public class Task {
+    private int id;
+    private String description;
+    private boolean completed;
+
+    
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Task [ID=" + id + ", Description=" + description + ", Completed=" + completed + "]";
+    }
+
+}
