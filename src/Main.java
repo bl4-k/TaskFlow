14bfefb49df75d3c77cf12a003d966c94eaad8d4
Toba <270317238+bl4-k@users.noreply.github.com>
2026-04-25 01:28:33 +0100

    feature: added cli interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        TaskManager manager = new TaskManager();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n1. Add Task");
+            System.out.println("2. List Tasks");
+            System.out.println("3. Complete Task");
+            System.out.println("4. Delete Task");
+            System.out.println("5. Exit");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            if (choice == 1) {
+                System.out.println("Task: ");
+                String desc = sc.nextLine();
+                manager.addTask(desc);
+
+            } else if (choice == 2) {
+                manager.listTasks();
+
+            } else if (choice == 3) {
+                System.out.println("Task ID: ");
+                int id = sc.nextInt();
+                manager.completeTask(id);
+            } else if (choice == 4) {
+                System.out.println("Task ID: ");
+                int id = sc.nextInt();
+                manager.deleteTask(id);
+            } else if (choice == 5) {
+                break;
+            }
+        }
+
+        sc.close();
 
     }
 }
